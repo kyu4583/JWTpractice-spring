4c6cb57e971729df206624a91af00ceb3f2d396c
Sungkyu <kxu4583@naver.com>
2023-12-23 23:55:04 +0900

    init

---
+++ src/main/java/JWTpractice/JWTpracticespring/JwTpracticeSpringApplication.java
@@ -0,0 +1,13 @@
+package JWTpractice.JWTpracticespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTpracticeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwTpracticeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JWTpractice/JWTpracticespring/JwTpracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package JWTpractice.JWTpracticespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTpracticeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
